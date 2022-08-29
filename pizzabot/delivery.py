4f0be551c2b75f06930fc7023e4bb99696634e2b
veranika-zakharchenia <v.zakharchenia@itechart-group.com>
2022-08-29 15:21:52 +0300

    Pizzabot-4: implement delivery logic

--- pizzabot/delivery.py
+++ pizzabot/delivery.py
@@ -1,22 +1,55 @@
-STARTING_POINT = (0, 0)
+from typing import List
+
+from pizzabot.point import Point
+
+STARTING_POINT = Point(0, 0)
 COMMAND_DROP = 'D'
 COMMAND_EAST = 'E'
 COMMAND_WEST = 'W'
 COMMAND_SOUTH = 'S'
 COMMAND_NORTH = 'N'
 
+
 class Delivery:
-    def __init__(self, points):
+    def __init__(self, points: List[Point]):
         self.points = points
 
-    def _move_horizontally(self):
-        pass
+    def _move_horizontally(self, starting_coordinate: int, finish_coordinate: int):
+        horizontal_movement = ''
 
-    def _move_vertically(self):
-        pass
+        diff_x = starting_coordinate - finish_coordinate
+        if diff_x > 0:
+            direction_x = COMMAND_WEST
+        else:
+            direction_x = COMMAND_EAST
 
-    def _get_intermediate_instructions(self, prev_point, current_point):
-        pass
+        for i in range(0, abs(diff_x)):
+            horizontal_movement += direction_x
+
+        return horizontal_movement
+
+    def _move_vertically(self, starting_coordinate: int, finish_coordinate: int):
+        vertical_movement = ''
+
+        diff_y = starting_coordinate - finish_coordinate
+        if diff_y > 0:
+            direction_y = COMMAND_SOUTH
+        else:
+            direction_y = COMMAND_NORTH
+
+        for i in range(0, abs(diff_y)):
+            vertical_movement += direction_y
+
+        return vertical_movement
+
+    def _get_intermediate_instructions(self, starting_point: Point, finishing_point: Point) -> str:
+        intermediate_instructions = ''
+
+        intermediate_instructions += self._move_horizontally(starting_point.x, finishing_point.x)
+        intermediate_instructions += self._move_vertically(starting_point.y, finishing_point.y)
+        intermediate_instructions += COMMAND_DROP
+
+        return intermediate_instructions
 
     def get_instructions(self) -> str:
         instructions = ''
